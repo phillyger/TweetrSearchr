8446896b9e4e7173e54e2d81cd67aaf741cffc29
Ger O'Sullivan <ger@brilliantage.com>
2017-11-21 21:58:52 -0500

    adding models

---
+++ app/src/main/java/com/brilliantage/apps/android/tweetr/model/Tweet.kt
@@ -0,0 +1,7 @@
+package com.brilliantage.apps.android.tweetr.model
+
+/**
+ * Created by gosullivan on 11/21/17.
+ */
+class Tweet {
+}
---
+++ app/src/main/java/com/brilliantage/apps/android/tweetr/model/Tweets.kt
@@ -0,0 +1,7 @@
+package com.brilliantage.apps.android.tweetr.model
+
+/**
+ * Created by gosullivan on 11/21/17.
+ */
+data class Tweets(val tweets:ArrayList<Tweet>) {
+}
