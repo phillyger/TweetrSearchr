534300511f28f8a190d0c8e98afae8e2e1288283
Ger O'Sullivan <ger@brilliantage.com>
2017-11-24 13:05:30 -0500

    - Adding in an extension methods for handling the creation of the Gson converter object

--- app/src/main/java/com/brilliantage/apps/android/tweetr/repositories/providers/extension/GsonAndRxJava.kt
+++ app/src/main/java/com/brilliantage/apps/android/tweetr/repositories/providers/extension/GsonAndRxJava.kt
@@ -2,10 +2,16 @@ package com.brilliantage.apps.android.tweetr.repositories.providers.extension
 
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-
+import retrofit2.converter.gson.GsonConverterFactory
+import com.google.gson.GsonBuilder
 /**
  * Created by gosullivan on 11/21/17.
  */
 
+// add extension method for Call Adapters
 fun Retrofit.Builder.addCallAdapters():Retrofit.Builder = this
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+
+// add extension method for Cconverters
+fun Retrofit.Builder.addConverters(): Retrofit.Builder = this
+        .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
