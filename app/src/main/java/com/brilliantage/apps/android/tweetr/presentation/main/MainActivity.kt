1fa5cf43c66f2e7c3284cb67c140b8573bbd06ca
Ger O'Sullivan <ger@brilliantage.com>
2017-11-26 17:20:21 -0500

    - refactored searchTweets to allow for infinite scrolling by extending the method with maxId and includeEntities params. - added conditional check in the onSuccess event of searchTweets to check for valid itemCount. - added method to split query string in order to ensure loadMoreTweets call would pass appropriate params to the searchTweet method.

--- app/src/main/java/com/brilliantage/apps/android/tweetr/presentation/main/MainActivity.kt
+++ app/src/main/java/com/brilliantage/apps/android/tweetr/presentation/main/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.brilliantage.apps.android.tweetr.presentation.main
 
 import android.os.Bundle
+
+import android.support.v4.view.MenuItemCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.SearchView
+import android.support.v7.widget.Toolbar
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import com.brilliantage.apps.android.tweetr.R
-import com.brilliantage.apps.android.tweetr.model.search.SearchMetadata
-import com.brilliantage.apps.android.tweetr.model.search.Status
 import com.brilliantage.apps.android.tweetr.presentation.base.PresenterBaseActivity
-import com.marcinmoskala.kotlinandroidviewbindings.*
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 /**
  * Created by gosullivan on 11/22/17.
@@ -18,20 +23,12 @@ class MainActivity : PresenterBaseActivity(), MainView {
 
     val TAG:String = "MainActivity"
 
-//    private var searchStatusList:ArrayList<Status> = ArrayList<Status>()
-//    private var searchMetaData:SearchMetadata? = null
     private lateinit var searchAdapter: SearchAdapter
     private lateinit var linearLayoutManager: LinearLayoutManager
 
-    private val lastVisibleItemPosition: Int
-        get() = linearLayoutManager.findLastVisibleItemPosition()
 
 
-//    override var progressVisible by bindToLoading(R.id.progressView, R.id.searchResultsView)
-
-//    override var query: String by bindToEditText(R.id.searchInputView)
-
-//    override var searchButtonClickedCallback by bindToClick(R.id.searchButton)
+//    override var query: String by bindToEditText(R.id.query)
 
     val mainPresenter by presenter { MainPresenter(this) }
 
@@ -39,30 +36,121 @@ class MainActivity : PresenterBaseActivity(), MainView {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initToolbar()
+        initView()
+    }
+
+
+    /*
+    * Initialize the recycler view
+    * */
+    private fun initView() {
         linearLayoutManager = LinearLayoutManager(this)
 
         searchAdapter = SearchAdapter(mainPresenter as MainPresenter)
-        searchResults.adapter = searchAdapter
-        searchResults.layoutManager = linearLayoutManager
+        searchResultsView.adapter = searchAdapter
+        searchResultsView.layoutManager = linearLayoutManager
 
-        setRecyclerViewScrollListener()
+        setRecyclerViewScrollListener(linearLayoutManager)
     }
 
-    override fun updateUI() {
+    /*
+     * Initialize the toolbar
+     * */
+    private fun initToolbar() {
+        val toolbar = toolbar as Toolbar
+        setSupportActionBar(toolbar)
+    }
+
+
+    /*
+    * Update the UI with the new result set
+    * */
+    override fun updateUI(positionStart:Int, itemCount:Int) {
+        // Update the search adapter dataset on the main UI thread
+
+        progressView.visibility = View.INVISIBLE
+        searchResultsView.visibility = View.VISIBLE
+
         runOnUiThread {
-             searchAdapter.notifyDataSetChanged()
+            searchAdapter.notifyItemRangeInserted(positionStart,itemCount)
+
+
         }
     }
 
-    private fun setRecyclerViewScrollListener() {
-        searchResults.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
-                super.onScrollStateChanged(recyclerView, newState)
-                val totalItemCount = searchResults!!.layoutManager.itemCount
-                if (!(mainPresenter as MainPresenter).isLoadingData  && totalItemCount == lastVisibleItemPosition + 1) {
-                    (mainPresenter as MainPresenter).loadMoreTweets()
-                }
+    /*
+    *  Sets a listener on the recyclerview scroll event
+    * */
+    private fun setRecyclerViewScrollListener(layoutMgr:LinearLayoutManager) {
+
+        searchResultsView.addOnScrollListener(object : EndlessRecyclerViewScrollListener(layoutMgr) {
+            override fun onLoadMore(page: Int, totalItemsCount: Int, view: RecyclerView?) {
+                // Triggered only when new data needs to be appended to the list
+                // Add whatever code is needed to append new items to your AdapterView
+                loadMoreTweets(page)
             }
         })
+    }
+
+
+    // Append the next page of data into the adapter
+    // This method probably sends out a network request and appends new data items to your adapter.
+    fun loadMoreTweets(offset: Int) {
+        // Send an API request to retrieve appropriate paginated data
+        //  --> Send the request including an offset value (i.e `page`) as a query parameter.
+        //  --> Deserialize and construct new model objects from the API response
+        //  --> Append the new data objects to the existing set of items inside the array of items
+        //  --> Notify the adapter of the new items made with `notifyItemRangeInserted()`
+
+        (mainPresenter as MainPresenter).loadMoreTweets()
+    }
+
+    /*
+     * Add in the Search menu option.
+    * */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        val search:MenuItem? = menu?.findItem(R.id.search) as? MenuItem
+
+        search?.let {
+            val searchView:SearchView? = MenuItemCompat.getActionView(it) as? SearchView
+            searchView?.let {
+                it.setQuery("", false)
+                search(it)
+            }
+
+        }
+
+        return true
+    }
+
+    /*
+     *  Perform the search
+    * */
+    private fun search(searchView: SearchView) {
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+
+                progressView.visibility = View.VISIBLE
+                searchResultsView.visibility = View.INVISIBLE
+
+                Log.d(TAG,"Search Text: $query")
+                (mainPresenter as MainPresenter).resetSearch()
+                (mainPresenter as MainPresenter).searchTweets(query, null, null)
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                Log.d(TAG,"Search Text: $newText")
+                return true
+            }
+
+
+        })
+
     }
 }
